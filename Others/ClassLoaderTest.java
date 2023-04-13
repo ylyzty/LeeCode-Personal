9a16493df7262f811fc84fe74a33e7bdfa7ede13
zyli <2436472829@qq.com>
2023-04-13 21:36:10 +0800

    ClassLoader define

--- Others/ClassLoaderTest.java
+++ Others/ClassLoaderTest.java
@@ -1,7 +1,15 @@
 package Others;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ClassLoaderTest extends ClassLoader{
 
+    /**
+     * 类加载路径
+     */
     private String path;
 
     public ClassLoaderTest(String path) {
@@ -11,20 +19,43 @@ public class ClassLoaderTest extends ClassLoader{
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        // TODO Auto-generated method stub
-        Class c = null;
-        
-        // 自定义获取 class 文件的字节码数组
-        byte[] classData = getClassData();
-        if (classData != null) {
-            // 创建 class
-            c = defineClass(name, classData, 0, classData.length);
+        String fileName = getFileName(name);
+        File file = new File(path, fileName);
+
+        try {
+            // 获取 class 文件, 读取到内存中
+            FileInputStream fis = new FileInputStream(file);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            int length = 0;
+            try {
+                while ((length = fis.read()) != -1) {
+                    baos.write(length);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            byte[] data = baos.toByteArray();
+            fis.close();
+            baos.close();
+
+            return defineClass(name, data, 0, data.length);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return c;
+        // 加载不了则 ClassLoader.findClass方法, 抛出异常
+        return super.findClass(name);
     }
 
-    private byte[] getClassData() {
-        return null;
+    private String getFileName(String name) {
+        int index = name.lastIndexOf('.');
+        if (index == -1) {
+            return name + ".class";
+        }
+        else {
+            return name.substring(index + 1) + ".class";
+        }
     }
 }
